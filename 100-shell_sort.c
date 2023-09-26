925565c2f243306f32a727a201a75155145263da
Brendamasingu <brendamasingu@gmail.com>
2023-09-25 20:30:49 -0700

    betty fixed

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * shell_sort - function that sorts an array
+ * of integers in ascending order using the
+ * Shell sort algorithm
+ *
+ * @array: input arrray
+ * @size: size of the array
+ * Return: no return
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t n = 1;
+
+	while (n < size)
+		n = (n * 3) + 1;
+
+	while ((n = (n - 1) / 3) > 0)
+		_ssort(array, size, n);
+}
+/**
+ * _ssort - auxiliar function for
+ * shell_sort function
+ * @a: input arrray
+ * @size: size of the array
+ * @n: intervale
+ * Return: no return
+ */
+void _ssort(int *a, int size, int n)
+{
+	int tmp, s, b;
+
+	for (s = 0; (s + n) < size; s++)
+	{
+		for (b = s + n; (b - n) >= 0; b = b - n)
+		{
+			if (a[b] < a[b - n])
+			{
+				tmp = a[b];
+				a[b] = a[b - n];
+				a[b - n] = tmp;
+			}
+		}
+	}
+	print_array(a, size);
+}
