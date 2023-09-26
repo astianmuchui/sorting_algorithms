d66ad0356785f6f68275d7d294a7a45512960bd2
astianmuchui <astianmuchui@gmail.com>
2023-09-26 05:48:26 +0300

    updated header file

--- sort.h
+++ sort.h
@@ -27,5 +27,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void exec_quicksort(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
